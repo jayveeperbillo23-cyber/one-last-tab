66e17d9a512e71d224ddcdcb6728ce84ab4fc789
jayveeperbillo23-cyber <jayveeperbillo23@gmail.com>
2026-07-31 22:45:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+const chapters = [
+    {
+        chapter: "Chapter I",
+        title: "Hi, Tiffany.",
+        text: "Thank you for being here."
+    },
+
+    {
+        chapter: "Chapter II",
+        title: "I wasn't sure if I should do this.",
+        text: "But if I never did...\n\nI'd probably regret it."
+    },
+
+    {
+        chapter: "Chapter III",
+        title: "I don't know\nexactly\nwhen it happened.\n\nOne day...\n\nI just realized...\n\nI looked forward\nto seeing you."
+    },
+
+    {
+        chapter: "Chapter IV",
+        title: "You probably\nnever noticed.",
+        text: "But\n\nyou became\n\npart of\n\nmy ordinary days."
+    },
+
+    {
+        chapter: "Chapter V",
+        title: "It wasn't\n\njust your smile.",
+        text: "...\n\nOr your laugh.\n\n...\n\nOr your kindness.\n\n...\n\nIt was...\n\nyou."
+    },
+
+    {
+        chapter: "",
+        title: "I like you.",
+        text: "And...\n\nthat's the truth."
+    },
+
+    {
+        chapter: "",
+        title: "Whether\n\nthis changes\n\nanything...\n\nor not...",
+        text: "I'm still glad\n\nI told you."
+    },
+
+    {
+        chapter: "",
+        title: "Thank you...",
+        text: "for reading\n\nmy heart."
+    },
+
+    {
+        chapter: "",
+        title: "With sincerity,",
+        text: "— Jayvee\n\n\nSome feelings deserve honesty,\neven without certainty."
+    }
+];
+
 const title = document.getElementById("title");
 const subtitle = document.getElementById("subtitle");
 const button = document.getElementById("continueBtn");
